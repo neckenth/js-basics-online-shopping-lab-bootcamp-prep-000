282128ede960ae5801b177dcbec6b2e5af963314
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-12-03 20:14:35 +0000

    automatically backed up by learn

--- indexAgainAgain.js
+++ indexAgainAgain.js
@@ -44,7 +44,7 @@ function total() {
 function removeFromCart(item) {
     for (var i in cart) {
         if (cart[i].hasOwnProperty(item)) {
-            cart.splice(cart.indexOf(item), 1)
+            cart.splice(i, 1)
         }
     } console.log(`That item is not in your cart.`)
     return cart
